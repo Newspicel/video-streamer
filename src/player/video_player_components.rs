76a703b3e9f565b7dd697423af7c47f5ea36046a
Julian Haag <me@jhaag.me>
2025-02-01 07:10:21 +0100

    Save

--- src/player/video_player_components.rs
+++ src/player/video_player_components.rs
@@ -1,9 +1,13 @@
+use std::time::Duration;
+
 use leptos::{
     ev::{click, keydown, mouseleave, mousemove, pause, play, progress, timeupdate},
     html::{Div, Video},
     prelude::*,
 };
-use leptos_use::{use_document, use_event_listener};
+use leptos_use::{
+    use_document, use_event_listener, use_timeout_fn, use_timestamp, UseTimeoutFnReturn,
+};
 use web_sys::{DomRect, Event, HtmlDivElement, HtmlVideoElement, ProgressEvent};
 
 #[component]
@@ -11,44 +15,30 @@ pub fn VideoPlayerControll(
     container_ref: NodeRef<Div>,
     video_ref: NodeRef<Video>,
 ) -> impl IntoView {
-    let (show_controls, set_show_controls) = signal(true);
-    let last_mouse_move = ::new(None::<f64>);
+    let (show_controls, set_show_controls) = signal(false);
+    let last_mouse_move = StoredValue::new(use_timestamp().get_untracked());
 
-    // Function to show controls and reset the timer
-    let reset_timer = move || {
+    const SHOW_CONTROLS_TIMEOUT: f64 = 3000.0;
+
+    let UseTimeoutFnReturn { start, .. } = use_timeout_fn(
+        move |_| {
+            let last_move = last_mouse_move.get_value();
+            let current_time = use_timestamp().get_untracked();
+            if (current_time - last_move) > SHOW_CONTROLS_TIMEOUT {
+                set_show_controls(false);
+            }
+        },
+        SHOW_CONTROLS_TIMEOUT,
+    );
+
+    _ = use_event_listener(container_ref, mousemove, move |_| {
         set_show_controls(true);
-        last_mouse_move.set(Some(js_sys::Date::now()));
-
-        // Start a timeout to hide controls after 3 seconds
-        let hide_controls = set_timeout(
-            move || {
-                let now = js_sys::Date::now();
-                if let Some(last_move) = last_mouse_move.get() {
-                    if now - last_move > 3000.0 {
-                        set_show_controls(false);
-                    }
-                }
-            },
-            3000,
-        );
-
-        // Prevent race conditions
-        hide_controls.cancel();
-        hide_controls.run();
-    };
-
-    // Listen for mouse movement
-    _ = use_event_listener(use_document(), mousemove, move |_| {
-        reset_timer();
-    });
-
-    // Ensure controls stay visible when hovered
-    _ = use_event_listener(use_document(), mouseleave, move |_| {
-        set_show_controls(false);
+        last_mouse_move.set_value(use_timestamp().get_untracked());
+        start(());
     });
 
     view! {
-      <div class="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent 
+      <div class="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent
                   transition-opacity duration-300 ease-in-out"
            class:opacity-100=move || show_controls()
            class:opacity-0=move || !show_controls()
@@ -73,8 +63,8 @@ pub fn VideoPlayerControll(
               {/* Right Controls */}
               <div class="flex items-center gap-4">
                   <VideoPlayerControllInfo video_ref=video_ref/>
-                  <VideoPlayerControllSubtitle video_ref=video_ref/>
-                  <VideoPlayerControllSpeed video_ref=video_ref/>
+                  //<VideoPlayerControllSubtitle video_ref=video_ref/>
+                  <VideoPlayerControllOptions video_ref=video_ref/>
                   <VideoPlayerControllFullScreen container_ref=container_ref/>
               </div>
           </div>
@@ -240,7 +230,7 @@ fn VideoPlayerControllPlay(video_ref: NodeRef<Video>) -> impl IntoView {
     });
 
     view! {
-      <IconButton on_click=video_play>
+      <IconButton on:click= move |_| video_play()>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path fill-rule="evenodd" d={move || {
               if is_playing() {
@@ -259,7 +249,6 @@ fn VideoPlayerControllBackward(video_ref: NodeRef<Video>) -> impl IntoView {
     let action = move || {
         let video: HtmlVideoElement = video_ref.get_untracked().unwrap();
         let new_time = (video.current_time() - 10.0).max(0.0);
-        leptos::logging::log!("Set time to {}", new_time);
         video.set_current_time(new_time);
     };
 
@@ -270,7 +259,7 @@ fn VideoPlayerControllBackward(video_ref: NodeRef<Video>) -> impl IntoView {
     });
 
     view! {
-      <IconButton on_click=action>
+      <IconButton on:click= move |_| action()>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z" />
         </svg>
@@ -283,7 +272,6 @@ fn VideoPlayerControllForward(video_ref: NodeRef<Video>) -> impl IntoView {
     let action = move || {
         let video: HtmlVideoElement = video_ref.get_untracked().unwrap();
         let new_time = (video.current_time() + 10.0).min(video.duration());
-        leptos::logging::log!("Set time to {}", new_time);
         video.set_current_time(new_time);
     };
 
@@ -294,7 +282,7 @@ fn VideoPlayerControllForward(video_ref: NodeRef<Video>) -> impl IntoView {
     });
 
     view! {
-      <IconButton on_click=action>
+      <IconButton on:click= move |_| action()>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
         </svg>
@@ -335,7 +323,7 @@ fn VideoPlayerControllAudio(video_ref: NodeRef<Video>) -> impl IntoView {
     view! {
         <div class="flex group">
             {/* Mute Button */}
-            <IconButton on_click=mute>
+            <IconButton on:click= move |_| mute()>
                 <Show when=move || is_mute()>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                         <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM17.78 9.22a.75.75 0 1 0-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L20.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z" />
@@ -350,7 +338,7 @@ fn VideoPlayerControllAudio(video_ref: NodeRef<Video>) -> impl IntoView {
                 </Show>
             </IconButton>
 
-            {/* Volume Control Slider */}
+            {/* Volume Control Slider  */ }
             <div class="p-2 w-24 h-2">
                     <input
                         type="range"
@@ -378,7 +366,7 @@ fn VideoPlayerControllInfo(video_ref: NodeRef<Video>) -> impl IntoView {
     };
 
     view! {
-      <IconButton on_click=action>
+      <IconButton on:click= move |_| action()>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path d="M5.566 4.657A4.505 4.505 0 0 1 6.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0 0 15.75 3h-7.5a3 3 0 0 0-2.684 1.657ZM2.25 12a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3v-6ZM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 0 1 6.75 6h10.5a3 3 0 0 1 2.683 1.657A4.505 4.505 0 0 0 18.75 7.5H5.25Z" />
           </svg>
@@ -393,7 +381,7 @@ fn VideoPlayerControllSubtitle(video_ref: NodeRef<Video>) -> impl IntoView {
     };
 
     view! {
-      <IconButton on_click=action>
+      <IconButton on:click= move |_| action()>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
             <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z" clip-rule="evenodd" />
           </svg>
@@ -402,20 +390,107 @@ fn VideoPlayerControllSubtitle(video_ref: NodeRef<Video>) -> impl IntoView {
 }
 
 #[component]
-fn VideoPlayerControllSpeed(video_ref: NodeRef<Video>) -> impl IntoView {
-    let action = move || {
-        let _video: HtmlVideoElement = video_ref.get_untracked().unwrap();
+fn VideoPlayerControllOptions(video_ref: NodeRef<Video>) -> impl IntoView {
+    let (is_show_menu, set_show_menu) = signal(false);
+    let (settings_page, set_settings_page) = signal("main".to_string());
+
+    let open_menu = move || {
+        set_settings_page("main".to_string());
+        set_show_menu(true);
     };
 
+    let close_menu = move || {
+        set_show_menu(false);
+    };
+
+    let go_to_page = move |page: &str| set_settings_page(page.to_string());
+    let go_back = move || set_settings_page("main".to_string());
+
     view! {
-      <IconButton on_click=action>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
-          </svg>
-      </IconButton>
+        <Show when=move || is_show_menu()>
+            <div class="absolute bottom-24 right-4 w-60 bg-neutral-800/90 rounded-lg shadow-lg p-4 text-neutral-200">
+                <Show when=move || settings_page() == "main">
+                    <div>
+                        <div class="flex items-center justify-between mb-4">
+                            <p class="text-sm font-medium">Untertitel (1)</p>
+                            <button class="text-blue-500 text-sm" on:click=move |_| go_to_page("subtitles")>Aus ></button>
+                        </div>
+                        <div class="flex items-center justify-between mb-4">
+                            <p class="text-sm font-medium">Wiedergabegeschwindigkeit</p>
+                            <button class="text-blue-500 text-sm" on:click=move |_| go_to_page("playback_speed")>Benutzerdefiniert (1) ></button>
+                        </div>
+                        <div class="flex items-center justify-between mb-4">
+                            <p class="text-sm font-medium">Ruhemodus-Timer</p>
+                            <button class="text-blue-500 text-sm" on:click=move |_| go_to_page("sleep_timer")>Aus ></button>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <p class="text-sm font-medium">Qualität</p>
+                            <button class="text-blue-500 text-sm" on:click=move |_| go_to_page("quality")>Automatisch (1080p Premium HD) ></button>
+                        </div>
+                    </div>
+                </Show>
+
+                <Show when=move || settings_page() == "subtitles">
+                    <div>
+                        <div class="flex items-center mb-4">
+                            <button class="text-blue-500 text-sm mr-4" on:click=move |_| go_back()>{"<"}</button>
+                            <p class="text-sm font-medium">Untertitel</p>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">Deutsch</p>
+                                <input type="radio" name="subtitle" class="cursor-pointer" />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">Englisch</p>
+                                <input type="radio" name="subtitle" class="cursor-pointer" />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">Aus</p>
+                                <input type="radio" name="subtitle" class="cursor-pointer" checked=true />
+                            </div>
+                        </div>
+                    </div>
+                </Show>
+
+                <Show when=move || settings_page() == "playback_speed">
+                    <div>
+                        <div class="flex items-center mb-4">
+                            <button class="text-blue-500 text-sm mr-4" on:click=move |_| go_back()>{"<"}</button>
+                            <p class="text-sm font-medium">Wiedergabegeschwindigkeit</p>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">0.5x</p>
+                                <input type="radio" name="speed" class="cursor-pointer" />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">1.0x (Standard)</p>
+                                <input type="radio" name="speed" class="cursor-pointer" checked=true />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">1.5x</p>
+                                <input type="radio" name="speed" class="cursor-pointer" />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <p class="text-sm">2.0x</p>
+                                <input type="radio" name="speed" class="cursor-pointer" />
+                            </div>
+                        </div>
+                    </div>
+                </Show>
+            </div>
+        </Show>
+
+        <IconButton on:click=move |_| open_menu()>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clip-rule="evenodd" />
+            </svg>
+        </IconButton>
     }
 }
-
+            
+                
 #[component]
 fn VideoPlayerControllFullScreen(container_ref: NodeRef<Div>) -> impl IntoView {
     let (is_fullscreen, set_fullscreen) = signal(false);
@@ -441,7 +516,7 @@ fn VideoPlayerControllFullScreen(container_ref: NodeRef<Div>) -> impl IntoView {
     });
 
     view! {
-      <IconButton on_click=toggle_fullscreen>
+      <IconButton on:click= move |_| toggle_fullscreen()>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
               <path fill-rule="evenodd" d={move || {
                   if is_fullscreen() {
@@ -456,14 +531,10 @@ fn VideoPlayerControllFullScreen(container_ref: NodeRef<Div>) -> impl IntoView {
 }
 
 #[component]
-pub fn IconButton<F>(children: ChildrenFn, on_click: F) -> impl IntoView
-where
-    F: Fn() + 'static,
-{
+pub fn IconButton(children: ChildrenFn) -> impl IntoView{
     let button_ref = NodeRef::new();
     view! {
         <button node_ref=button_ref class="p-2 text-neutral-200 rounded-full hover:bg-neutral-800 focus:outline-none" on:click=move |_| {
-          on_click();
           _ = button_ref.get_untracked().expect("Failed to get button element").blur();
         }> {children()} </button>
     }
